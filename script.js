0615811566e3505adc6c83e8f9b8f90ab21d8199
ColomboRiccardo <riccardocolombopro@gmail.com>
2021-01-19 16:47:35 +0100

    added javascript functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//going to try some stuff
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//Prompt the user to select a media stream
+async function selectMediaStream() {
+	try {
+		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+		videoElement.srcObject = mediaStream;
+		videoElement.onloadedmetadata = () => {
+			videoElement.play();
+		};
+	} catch (error) {
+		console.log('whoops, error here: ', error);
+	}
+}
+
+button.addEventListener('click', async () => {
+	// disable the button
+	button.disabled = true;
+	//start picture in picture
+	await videoElement.requestPictureInPicture();
+	// reset button
+	button.disabled = false;
+});
+
+selectMediaStream();
